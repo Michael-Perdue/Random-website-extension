8e8794fa12fcf7ea19b24e6d387c5f62d399be61
Michael Perdue <michael0perdue@gmail.com>
2023-08-08 18:53:57 +0100

    made it on click open a random url from an array of them

--- popup.js
+++ popup.js
@@ -1,1 +1,5 @@
-console.log('This is a popup!');
+chrome.browserAction.onClicked.addListener(function(tab){
+    const urls = ["https://sliding.toys/mystic-square/8-puzzle/daily/","https://paint.toys/"]
+    let newUrl = urls[Math.floor(Math.random()*urls.length)]
+    chrome.tabs.create({ url:newUrl });
+});
