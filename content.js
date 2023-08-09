f4cefa7f457d46ace98a37c362b391e3b4f62521
Michael Perdue <michael0perdue@gmail.com>
2023-08-09 08:57:59 +0100

    migrate over to manifest v3

--- content.js
+++ content.js
@@ -1,9 +1,8 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    document.getElementById("btn").addEventListener("click", randomSite);
+});
 function randomSite() {
     const urls = ["https://sliding.toys/mystic-square/8-puzzle/daily/","https://paint.toys/"];
     let newUrl = urls[Math.floor(Math.random()*urls.length)];
     chrome.tabs.create({ url:newUrl });
 }
-
-chrome.runtime.onInstalled.addListener(randomSite);
-
-
