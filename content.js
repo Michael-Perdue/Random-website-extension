f848ed5f7179a156bdaed68bd55bfb35b5f70281
Michael Perdue <michael0perdue@gmail.com>
2023-08-09 10:01:17 +0100

    loads the urls from the data.json file now

--- content.js
+++ content.js
@@ -1,8 +1,15 @@
 window.addEventListener("DOMContentLoaded", (event) => {
     document.getElementById("btn").addEventListener("click", randomSite);
 });
+let urls = []
+const url = chrome.runtime.getURL("/data.json");
+
+fetch(url)
+    .then((response) => response.json())
+    .then((json) => {console.log(json);urls = json.urls;console.log(urls)})
+    .catch((error) => console.log("fetch error: ",error))
+
 function randomSite() {
-    const urls = ["https://sliding.toys/mystic-square/8-puzzle/daily/","https://paint.toys/"];
     let newUrl = urls[Math.floor(Math.random()*urls.length)];
     chrome.tabs.create({ url:newUrl });
 }
